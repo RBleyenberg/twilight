68473e180edf11bb37721d20a79efba860d034bf
remco <remcobleijenberg@gmail.com>
2018-09-07 06:25:11 +0200

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SettingsContainerComponent } from './settings';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'settings',
+    component: SettingsContainerComponent,
+    data: { title: 'dare.menu.settings' }
+  },
+  {
+    path: '**',
+    redirectTo: 'about'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: false,
+      scrollPositionRestoration: 'enabled'
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
